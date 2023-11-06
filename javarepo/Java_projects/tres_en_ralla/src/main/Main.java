e66a538f8e3ce1b7bf43d4da7c66eb3046e8ccf7
María de los Ángeles Molina Díaz <mariangelesmolina1995@gmail.com>
2023-11-06 13:06:19 +0100

    update

--- javarepo/Java_projects/tres_en_ralla/src/main/Main.java
+++ javarepo/Java_projects/tres_en_ralla/src/main/Main.java
@@ -15,29 +15,91 @@ public class Main {
 		System.out.println("\n"); // Salto de línea
 	}
 
-	public static void tableroVacio() {
-		String[] lineax1 = { "| ", " ", " | ", "1", " | ", "2", " | ", "3", " | " };
-		String[] lineax2 = { "| ", "1", " | ", " ", " | ", " ", " | ", " ", " | " };
-		String[] lineax3 = { "| ", "2", " | ", " ", " | ", " ", " | ", " ", " | " };
-		String[] lineax4 = { "| ", "3", " | ", " ", " | ", " ", " | ", " ", " | " };
-
-		imprimirLinea(lineax1);
-		imprimirLinea(lineax2);
-		imprimirLinea(lineax3);
-		imprimirLinea(lineax4);
+	public static void tablero(String[] l1, String[] l2, String[] l3, String[] l4) {
+		System.out.println("\n___ TABLERO ___\n");
+		imprimirLinea(l1);
+		imprimirLinea(l2);
+		imprimirLinea(l3);
+		imprimirLinea(l4);
 		System.out.println("\n"); // Salto de línea
+	}
 
+	public static int[] recogerCoordenadas() {
+		Scanner sc = new Scanner(System.in);
+		int[] coordenadas = new int[2];
+		System.out.print("Introduce la coordenada x: ");
+		coordenadas[0] = sc.nextInt();
+		System.out.print("Introduce la coordenada y: ");
+		coordenadas[1] = sc.nextInt();
+		return coordenadas;
+	}
+
+	public static String[] insertarCoordenadas(int[] coord, String simbol, String[] linea) {
+		if (coord[1] == 1) {
+			if (linea[3].equals(" ")) {
+				linea[3] = simbol;
+			} else {
+				System.out.println("Estas posiciones ya estan señaladas.");
+			}
+		} else if (coord[1] == 2) {
+			if (linea[5].equals(" ")) {
+				linea[5] = simbol;
+			} else {
+				System.out.println("Estas posiciones ya estan señaladas.");
+			}
+		} else if (coord[1] == 3) {
+			if (linea[7].equals(" ")) {
+				linea[7] = simbol;
+			} else {
+				System.out.println("Estas posiciones ya estan señaladas.");
+			}
+		} else {
+			System.out.println("Estas coordenadas no son válidas");
+		}
+
+		return linea;
+	}
+
+	public static void imprimirCoordenadas(int[] coord, String simbol, String[] l1, String[] l2, String[] l3,
+			String[] l4) {
+		int key = coord[0];
+		switch (key) {
+		case 1:
+			// Llamo a la funcion para que inserte el simbolo en la columna que toca
+			l2 = insertarCoordenadas(coord, simbol, l2);
+			break;
+		case 2:
+			l3 = insertarCoordenadas(coord, simbol, l3);
+			break;
+		case 3:
+			l4 = insertarCoordenadas(coord, simbol, l4);
+			break;
+		default:
+			throw new IllegalArgumentException("Estas coordenadas no son válidas.");
+		}
+		tablero(l1, l2, l3, l4);
 	}
 	
-	public static int[] recogerCoordenadas () {
-		Scanner sc = new Scanner(System.in);
-		int[] coordenadas = new int [2];
-		System.out.print("Introduce la coordenada x: ");
-		coordenadas [0] = sc.nextInt();
-		System.out.print("Introduce la coordenada y: ");
-		coordenadas [1] = sc.nextInt();
+	public static boolean comprobacion (String[] l1, String[] l2, String[] l3,
+			String[] l4) {
+		boolean lleno = false;
+		boolean ganador = false;
+		if (l1[3] != " " && l1[5] != " " && l1[7] != " "
+				&& l2[3] != " " && l2[5] != " " && l2[7] != " "
+				&& l3[3] != " " && l3[5] != " " && l3[7] != " ") {
+			// Está el tablero lleno
+			lleno = true;
+		}
+		else if (ganador) {
+			// TODO implementar casos de éxito
+		}
+			
 		
-		return coordenadas;
+		
+		
+		
+		
+		return true;
 	}
 
 	public static void main(String[] args) {
@@ -45,7 +107,12 @@ public class Main {
 
 		int op = 0;
 		Scanner sc = new Scanner(System.in);
-		
+		int[] coord = new int[2];
+		String[] lineax1 = { "| ", " ", " | ", "1", " | ", "2", " | ", "3", " | " };
+		String[] lineax2 = { "| ", "1", " | ", " ", " | ", " ", " | ", " ", " | " };
+		String[] lineax3 = { "| ", "2", " | ", " ", " | ", " ", " | ", " ", " | " };
+		String[] lineax4 = { "| ", "3", " | ", " ", " | ", " ", " | ", " ", " | " };
+
 		System.out.println("******************");
 		System.out.println("** TRES EN RAYA **");
 		System.out.println("******************\n");
@@ -56,13 +123,13 @@ public class Main {
 
 		switch (op) {
 		case 1:
-			System.out.println("\n___ TABLERO ___\n");
-			tableroVacio();
+			tablero(lineax1, lineax2, lineax3, lineax4);
 			System.out.println("x __ JUGADOR 1 __ x");
-			recogerCoordenadas();
-			System.out.println("\no __ JUGADOR 2 __ o");
-			recogerCoordenadas();
-			
+			coord = recogerCoordenadas();
+			imprimirCoordenadas(coord, "x", lineax1, lineax2, lineax3, lineax4);
+			System.out.println("o __ JUGADOR 2 __ o");
+			coord = recogerCoordenadas();
+			imprimirCoordenadas(coord, "o", lineax1, lineax2, lineax3, lineax4);
 			break;
 
 		case 2:
